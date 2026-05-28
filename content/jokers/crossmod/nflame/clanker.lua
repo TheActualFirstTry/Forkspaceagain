739be3b062c3a67311813c65fc53001f5b17ab5c
etherware-novice <73374039+etherware-novice@users.noreply.github.com>
2026-05-28 05:41:50 -0500

    clanking code

--- content/jokers/crossmod/nflame/clanker.lua
+++ content/jokers/crossmod/nflame/clanker.lua
@@ -2,10 +2,7 @@ SMODS.Joker {
   key = "clanker",
   atlas = "placeholder",
   pos = { x = 0, y = 0 },
-  config = { extra = {
-    t_chips = 15,
-    t_chips_gain = 15
-  } },
+  config = { extra = { t_chips = 15 } },
   rarity = 2,
   cost = 8,
   blueprint_compat = true,
@@ -15,17 +12,13 @@ SMODS.Joker {
   pools = { silicon = true },
 
   loc_vars = function(self, info_queue, card)
-    return {
-      vars = {
-        card.ability.extra.t_chips * (G.nflame_get_silicontotal() - 1),
-        card.ability.extra.t_chips_gain
-      }
-    }
+    local silicount = G.nflame_get_silicontotal() or 0
+    return { vars = { card.ability.extra.t_chips, card.ability.extra.t_chips * silicount } }
   end,
 
   calculate = function(self, card, context)
     if context.individual and context.cardarea == G.play and context.other_card:is_suit("Spades") then
-      chips = t_chips + (t_chips_gain * G.nflame_get_silicontotal())
+      chips = t_chips * G.nflame_get_silicontotal(context)
     end
   end
 }
